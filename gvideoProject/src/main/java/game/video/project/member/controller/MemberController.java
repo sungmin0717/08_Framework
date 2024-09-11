29582436e922d4c6c62c57f22f3fdf0dabc06465
DESKTOP-IEKEC4V\김성민 <sungmin48601@gmail.com>
2024-09-12 00:57:56 +0900

    0912

--- gvideoProject/src/main/java/game/video/project/member/controller/MemberController.java
+++ gvideoProject/src/main/java/game/video/project/member/controller/MemberController.java
@@ -11,5 +11,4 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberController {
 
 	
-	public String 
 }
--- gvideoProject/src/main/java/game/video/project/member/dto/Member.java
+++ gvideoProject/src/main/java/game/video/project/member/dto/Member.java
@@ -2,4 +2,11 @@ package game.video.project.member.dto;
 
 public class Member {
 
+	private int userNo;
+	private String userName;
+	private String userEmail;
+	private String userPw;
+	private String userDate;
+	private char userRemove;
+	
 }
