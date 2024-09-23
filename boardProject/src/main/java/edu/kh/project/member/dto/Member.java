58e0dcce5a1cd20dea0723cfa87fab523b8df51b
sungmin0717 <sungmin48601@gmail.com>
2024-09-23 17:49:31 +0900

    0923 학원

--- boardProject/src/main/java/edu/kh/project/member/dto/Member.java
+++ boardProject/src/main/java/edu/kh/project/member/dto/Member.java
@@ -7,18 +7,16 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-//Lombok : Java 개발시 자주 사용하는 구문을 자동 완성하는
-// 컴파일 시 자동 추가하는 라이브러리
+// Lombok : Java 개발 시 자주 사용하는 구문을 
+//	컴파일 시 자동 추가하는 라이브러리
 
 @Getter
 @Setter
 @NoArgsConstructor
-@ToString
 @AllArgsConstructor
-@Builder // 빌더 패턴  : 객체 생성 + 초기화 쉽게
-
+@ToString
+@Builder // 빌더 패턴 : 객체 생성 + 초기화를 쉽게하는 패턴
 public class Member {
-
   private int 		memberNo;
   private String 	memberEmail;
   private String 	memberPw;
